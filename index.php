f03a80c7a748b6ecdaa3e2b5aa51d0bafb4d39d6
MartinCalozo <martincalozo@gmail.com>
2021-07-30 17:38:34 -0300

    Giteando la pagina comun

---
+++ index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@700&display=swap" rel="stylesheet">
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="imagenes/logo.png" class="logo-brand" alt="logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" id="home">Home </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" id="portfolio">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" id="aboutUs">About us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" id="pricing">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" id="testimonial">Testimonial</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" id="contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <section id="hero">
+            <div id="container">
+                <div class="content-center topmargin-lg">    
+                    <h1>WebSite Desing and Developer</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, odit nesciunt aperiam magni quibusdam unde ea incidunt consectetur consequuntur molestiae dicta quae suscipit, maiores eaque dolore vel. Iste, repellat eos.</p>
+                    <a href="#" class="btn btn-light topmargin-sm">Explore now <!--<ion-icon class="icon ion-md-arrow-dropright-circle"name="play-circle-outline"></ion-icon>--></a>    
+                </div>
+            </div>
+        </section>
+        <section id="portafolio">
+            <div class="container-fluid">
+                <div class="content-center">
+                    <h2>We work exciting brands to build <b>amazing products</b></h2>
+                    <p>Lorem ipsum dolor sietur adipisicing elit. Ut voluptates ullam, ad aspernatur odio explicabo praesentium 
+                        que optio doloribus id nobiam hic necessitatibus laudantium sint cupiditate voluptas!</p>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="portafolio-container">
+                            <div class="portafolio-details">
+                                <a href="#">
+                                    <h2>keep Calm and Eat</h2>
+                                </a>
+                                <a href="#">
+                                    <p>app/digital Productos</p> 
+                                </a>
+                            </div>
+                            <img src="imagenes/hero.jpg" class="img-fluid" alt="portafolio 1">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="portafolio-container">
+                            <div class="portafolio-details">
+                                <a href="#">
+                                    <h2>keep Calm and Eat</h2>
+                                </a>
+                                <a href="#">
+                                    <p>app/digital Productos</p> 
+                                </a>
+                            </div>
+                            <img src="imagenes/hero.jpg" class="img-fluid" alt="portafolio 1">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="portafolio-container">
+                            <div class="portafolio-details">
+                                <a href="#">
+                                    <h2>keep Calm and Eat</h2>
+                                </a>
+                                <a href="#">
+                                    <p>app/digital Productos</p> 
+                                </a>
+                            </div>
+                            <img src="imagenes/hero.jpg" class="img-fluid" alt="portafolio 1">
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="portafolio-container">
+                            <div class="portafolio-details">
+                                <a href="#">
+                                    <h2>keep Calm and Eat</h2>
+                                </a>
+                                <a href="#">
+                                    <p>app/digital Productos</p> 
+                                </a>
+                            </div>
+                            <img src="imagenes/hero.jpg" class="img-fluid" alt="portafolio 1">
+                        </div>
+                    </div>
+            </div>
+            
+            <div class="text-center topmargin-sm">
+                <p>Lorem ipsum dolor sit amet.</p>
+                <a href="#" class="text-dark">Lorem ipsum dolor sit.</a>
+            </div>
+        </section>    
+
+        <section id="aboutus" class="bg-light-grey">
+            <div class="container">
+            <div class="content-center">
+                    <h2>WebSite Desing and Developer</h2>
+                    <p>Lorem ipsum dolor sietur adipisicing elit. Ut voluptates ullam, ad aspernatur odio explicabo praesentium 
+                        que optio doloribus id nobiam hic necessitatibus laudantium sint cupiditate voluptas!</p>
+                </div>
+            </div>
+        </section>  
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
